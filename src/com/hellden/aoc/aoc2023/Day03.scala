f1a9e85dae09a7d29b7a414f5ae7f7d49844b8c4
kaspar <kaspar@recordedfuture.com>
2023-12-03 12:53:06 +0100

    day 3 refactor

--- src/com/hellden/aoc/aoc2023/Day03.scala
+++ src/com/hellden/aoc/aoc2023/Day03.scala
@@ -1,35 +1,9 @@
 package com.hellden.aoc.aoc2023
 
-import com.hellden.aoc.aoc2023.Day03.EngineSchematic._
+import com.hellden.aoc.aoc2023.Day03.EngineSchematic
+import com.hellden.aoc.aoc2023.Day03.EngineSchematic.*
 
 object Day03 extends Day(3):
-
-  override def part1: Long = // 529618
-    val nonSymbols = "0123456789.".toSet
-    val grid = new Grid[Char](inputLines.map(_.toIndexedSeq).toIndexedSeq)
-
-    def isSymbol(c: Char): Boolean = !nonSymbols(c)
-
-    def isPartNumber(s: String, position: Position): Boolean =
-      grid.valueFor(position.move(dx = -1)).exists(isSymbol) ||
-      grid.valueFor(position.move(dx = s.length)).exists(isSymbol) ||
-      (-1 to s.length).flatMap(dx => grid.valueFor(position.move(dx = dx, dy = -1))).exists(isSymbol) ||
-      (-1 to s.length).flatMap(dx => grid.valueFor(position.move(dx = dx, dy = 1))).exists(isSymbol)
-
-    def sum(s: String, position: Position): Long =
-      if s.isEmpty then 0 else
-        val drop = s.takeWhile(x => !x.isDigit)
-        val digits = s.drop(drop.length).takeWhile(_.isDigit)
-        val partNumber = if isPartNumber(digits, position.move(dx = drop.length)) then digits.toLong else 0
-        val offset = drop.length + digits.length
-        partNumber + sum(s.drop(offset), position.move(dx = offset))
-
-    inputLines
-      .view
-      .zipWithIndex
-      .map: (line, y) =>
-        sum(line, Position(0, y))
-      .sum
 
   object EngineSchematic:
 
@@ -52,7 +26,7 @@ object Day03 extends Day(3):
         case Some(digit) if digit.isDigit =>
           val digits = s.takeWhile(_.isDigit)
           PartNumber(digits.toLong, position, digits.length) ::
-            findParts(s.drop(digits.length), position.move(dx = digits.length))
+          findParts(s.drop(digits.length), position.move(dx = digits.length))
         case Some(symbol) =>
           Symbol(symbol, position) :: findParts(s.drop(1), position.move(dx = 1))
 
@@ -64,19 +38,32 @@ object Day03 extends Day(3):
       .zipWithIndex
       .flatMap: (line, y) =>
         findParts(line, Position(0, y))
+    
+    val symbols: Seq[Symbol] = parts
+      .collect:
+        case s: Symbol => s
+    
+    val partNumbers: Seq[PartNumber] = parts
+      .collect:
+        case n: PartNumber => n
+
+  override def part1: Long = // 529618
+    partNumbers
+      .collect:
+        case partNumber if symbols.exists(s => adjacent(s.position, partNumber)) =>
+          partNumber.num
+      .sum
 
   override def part2: Long = // 77509019
 
     def ratioFor(gearPosition: Position): Long =
-      val partNumbers = EngineSchematic
-        .parts
+      val adjacentPartNumbers = partNumbers
         .collect:
-          case partNumber: PartNumber if adjacent(gearPosition, partNumber) =>
+          case partNumber if adjacent(gearPosition, partNumber) =>
             partNumber.num
-      if partNumbers.size > 1 then partNumbers.product else 0
+      if adjacentPartNumbers.size > 1 then adjacentPartNumbers.product else 0
 
-    EngineSchematic
-      .parts
+    symbols
       .collect:
         case Symbol('*', position) =>
           ratioFor(position)
