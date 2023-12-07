815618561bff9f496bcada86854238dcfab73119
kaspar <kaspar@recordedfuture.com>
2023-12-07 21:26:51 +0100

    day 7 cleanup

--- src/com/hellden/aoc/aoc2023/Day07.scala
+++ src/com/hellden/aoc/aoc2023/Day07.scala
@@ -1,58 +1,30 @@
 package com.hellden.aoc.aoc2023
 
-import com.hellden.aoc.aoc2023.Day07.Type._
-
 object Day07 extends Day(7):
 
-  enum Type:
-    case HighCard
-    case OnePair
-    case TwoPair
-    case ThreeOfKind
-    case FullHouse
-    case FourOfKind
-    case FiveOfKind
+  case class Hand(cards: String, bid: Long, sortValue: String)
+  given Ordering[Hand] = Ordering.by[Hand, String](_.sortValue)
 
-  object Type:
-    def typeFor(cardAmounts: Map[Char, Int], jokers: Int = 0): Type =
-      val orderedAmounts = cardAmounts.values.toSeq.sorted.reverse
-      val top = jokers + orderedAmounts.headOption.getOrElse(0)
-      val second = if orderedAmounts.size > 1 then orderedAmounts(1) else 0
-      (top, second) match
-        case (5, _) => FiveOfKind
-        case (4, _) => FourOfKind
-        case (3, 2) => FullHouse
-        case (3, _) => ThreeOfKind
-        case (2, 2) => TwoPair
-        case (2, _) => OnePair
-        case _ => HighCard
+  def sortValue(cards: String, cardOrder: String, joker: Option[Char]): String =
+    val orderedAmounts = cards.filterNot(joker.contains).groupBy(identity).values.map(_.length).toSeq.sorted.reverse
+    val topCardAmount = cards.count(joker.contains) + orderedAmounts.headOption.getOrElse(0)
+    val secondCardAmount = if orderedAmounts.size > 1 then orderedAmounts(1) else 0
+    val typeValue = (topCardAmount, secondCardAmount) match
+      case (3, 2) => 32
+      case (2, 2) => 22
+      case (x, _) => x * 10
+    typeValue.toString + cards.map(card => ('A' + cardOrder.indexOf(card)).toChar)
 
-  case class Hand(cards: String, bid: Long, `type`: Type)
-
-  def winnings(cardOrder: String, typeFor: String => Type): Long =
-
-    given Ordering[Hand] with
-      override def compare(x: Hand, y: Hand): Int =
-        x.`type`.ordinal - y.`type`.ordinal match
-          case 0 =>
-            x.cards.zip(y.cards)
-             .map { (xc, yc) => cardOrder.indexOf(xc) - cardOrder.indexOf(yc) }
-             .filter(_ != 0)
-             .head
-          case cmp => cmp
-
+  def winnings(cardOrder: String, joker: Option[Char] = None): Long =
     inputLines
       .map(_.split(' '))
-      .collect { case Array(cards, bid) => Hand(cards, bid.toLong, typeFor(cards)) }
+      .collect { case Array(cards, bid) => Hand(cards, bid.toLong, sortValue(cards, cardOrder, joker)) }
       .sorted.zipWithIndex
       .map { (hand, index) => hand.bid * (index + 1L) }
       .sum
 
-  def cardAmounts(cards: String): Map[Char, Int] =
-    cards.groupBy(identity).view.mapValues(_.length).toMap
-
   override def part1: Long = // 254024898
-    winnings("23456789TJQKA", cards => typeFor(cardAmounts(cards)))
+    winnings("23456789TJQKA")
 
   override def part2: Long = // 254115617
-    winnings("J23456789TQKA", cards => typeFor(cardAmounts(cards.filter(_ != 'J')), jokers = cards.count(_ == 'J')))
+    winnings("J23456789TQKA", joker = Some('J'))
